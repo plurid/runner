0cc16cd2713d0538bf5d6e11b53e6ee397549c6c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-21 07:04:41 +0200

    runner-javascript > dev: typings

--- packages/runner-javascript/source/typings.d.ts
+++ packages/runner-javascript/source/typings.d.ts
@@ -1,19 +1,24 @@
-export interface RunnerConfiguration<E> {
-    expected: E;
-    comparison?: RunnerComparison;
-    time?: RunnerTime;
-    timeless?: boolean;
-    name?: string;
-    message?: string;
+declare var runner: Runner.runner;
 
-    expect?: (result: E) => void;
+
+declare namespace Runner {
+    export interface RunnerConfiguration<E> {
+        expected: E;
+        comparison?: RunnerComparison;
+        time?: RunnerTime;
+        timeless?: boolean;
+        name?: string;
+        message?: string;
+
+        expect?: (result: E) => void;
+    }
+
+    export type RunnerComparison = ':' | '<' | '<:' | '>' | '>:';
+    export type RunnerTime = 'instant' | 'fast' | 'network' | 'network-slow' | 'network-fast';
+
+
+    export type runner = <R>(
+        run: () => R | Promise<R>,
+        configuration: RunnerConfiguration<R>,
+    ) => Promise<void>;
 }
-
-export type RunnerComparison = ':' | '<' | '<:' | '>' | '>:';
-export type RunnerTime = 'instant' | 'fast' | 'network' | 'network-slow' | 'network-fast';
-
-
-declare function runner<R>(
-    run: () => R | Promise<R>,
-    configuration: RunnerConfiguration<R>,
-): Promise<void>;
