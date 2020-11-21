517e3873f51c1d6f4ff00d7ce018b633afb40b84
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-11-21 06:12:11 +0200

    runner-javascript > dev: exports

---
+++ packages/runner-javascript/source/commands/index.ts
@@ -0,0 +1,13 @@
+// #region imports
+    // #region internal
+    import run from './run';
+    // #endregion internal
+// #endregion imports
+
+
+
+// #region exports
+export {
+    run,
+};
+// #endregion exports
--- packages/runner-javascript/source/index.ts
+++ packages/runner-javascript/source/index.ts
@@ -3,6 +3,8 @@
     import expect from '#functions/expect';
     import timeBenchmark from '#functions/timeBenchmark';
     import runner from '#functions/runner';
+
+    import * as commands from '#commands/index';
     // #endregion internal
 // #endregion imports
 
@@ -14,6 +16,8 @@ export * from '#data/interfaces';
 export {
     timeBenchmark,
     expect,
+
+    commands,
 };
 
 export default runner;
