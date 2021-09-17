2dc0fa3e7a98634f04f853dfa72a66c68421171f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-17 20:24:57 +0300

    runner-javascript > dev: callers/runner

--- packages/runner-javascript/source/callers/runner/index.ts
+++ packages/runner-javascript/source/callers/runner/index.ts
@@ -1,13 +1,86 @@
 // #region module
+export type Check = (
+    testValue: any,
+    expectedValue: any,
+    relationship: string,
+) => void;
+
+export type CheckTuple = [any, any, string, boolean];
+
+
 const runner = async <P = any, R = any>(
     prepare: () => Promise<P>,
-    run: (preparation: P) => Promise<R>,
+    run: (preparation: P, check: Check) => Promise<R>,
     postpare: (preparation: P, result: R) => Promise<void>,
 ) => {
     try {
+        let checks: CheckTuple[] = [];
+
+        const check: Check = (
+            testValue: any,
+            expectedValue: any,
+            relationship: string = '==',
+        ) => {
+            let passed = false;
+
+            switch (relationship) {
+                case '==':
+                    if (testValue === expectedValue) {
+                        passed = true;
+                    }
+                    break;
+                case '<':
+                    if (testValue < expectedValue) {
+                        passed = true;
+                    }
+                    break;
+                case '<=':
+                    if (testValue <= expectedValue) {
+                        passed = true;
+                    }
+                    break;
+                case '>':
+                    if (testValue > expectedValue) {
+                        passed = true;
+                    }
+                    break;
+                case '>=':
+                    if (testValue >= expectedValue) {
+                        passed = true;
+                    }
+                    break;
+                default:
+                    break;
+            }
+
+            checks.push([
+                testValue,
+                expectedValue,
+                relationship,
+                passed,
+            ]);
+        }
+
+
         const preparation = await prepare();
-        const result = await run(preparation);
+        const result = await run(preparation, check);
         await postpare(preparation, result);
+
+
+        for (const check of checks) {
+            const [
+                testValue,
+                expectedValue,
+                relationship,
+                passed,
+            ] = check;
+
+            if (passed) {
+                console.log(`test passed :: ${testValue.toString()} ${relationship} ${expectedValue.toString()}`)
+            } else {
+                console.log(`test failed :: ${testValue.toString()} not ${relationship} ${expectedValue.toString()}`)
+            }
+        }
     } catch (error) {
         console.log('runner error', error);
         return;
