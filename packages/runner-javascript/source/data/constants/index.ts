35dc46292525b7478e71654137d6cf011f8b0c99
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2021-09-19 09:29:42 +0300

    runner-javascript > dev: data/constants

--- packages/runner-javascript/source/data/constants/index.ts
+++ packages/runner-javascript/source/data/constants/index.ts
@@ -1,27 +1,5 @@
 // #region module
-// values in microseconds
-export const timeBenchmarkValues = {
-    'instant': parseInt(process.env.RUNNER_TIME_BENCHMARK_INSTANT || '') ?? 0,
-    'fast': parseInt(process.env.RUNNER_TIME_BENCHMARK_FAST || '') ?? 15_000,
-    'network': parseInt(process.env.RUNNER_TIME_BENCHMARK_NETWORK || '') ?? 500_000,
-    'network-slow': parseInt(process.env.RUNNER_TIME_BENCHMARK_NETWORK_SLOW || '') ?? 1500_000,
-    'network-fast': parseInt(process.env.RUNNER_TIME_BENCHMARK_NETWORK_FAST || '') ?? 300_000,
-};
-
-// value in microseconds
-export const timeTolerance =  parseInt(process.env.RUNNER_TIME_BENCHMARK_TOLERANCE || '') ?? 5_000;
-
-
-export const SILENT_PASS = process.env.RUNNER_SILENT_PASS === 'true';
-
-
-
-export const MAGENTA_BACKGROUND = '\x1b[45m%s\x1b[0m';
-export const RED_BACKGROUND = '\x1b[41m%s\x1b[0m';
-
-
-
-export const VERSION = '0.0.0-5';
+export const VERSION = '0.0.0-6';
 
 
 export const fileExtensions = [
@@ -34,4 +12,24 @@ export const runnerExtensions = [
     '.run',
     '.test',
 ];
+
+
+export const SILENT_PASS = process.env.RUNNER_SILENT_PASS === 'true';
+
+export const MAGENTA_BACKGROUND = '\x1b[45m%s\x1b[0m';
+export const RED_BACKGROUND = '\x1b[41m%s\x1b[0m';
+
+
+
+// values in microseconds
+export const timeBenchmarkValues = {
+    'instant': parseInt(process.env.RUNNER_TIME_BENCHMARK_INSTANT || '') ?? 0,
+    'fast': parseInt(process.env.RUNNER_TIME_BENCHMARK_FAST || '') ?? 15_000,
+    'network': parseInt(process.env.RUNNER_TIME_BENCHMARK_NETWORK || '') ?? 500_000,
+    'network-slow': parseInt(process.env.RUNNER_TIME_BENCHMARK_NETWORK_SLOW || '') ?? 1500_000,
+    'network-fast': parseInt(process.env.RUNNER_TIME_BENCHMARK_NETWORK_FAST || '') ?? 300_000,
+};
+
+// value in microseconds
+export const timeTolerance =  parseInt(process.env.RUNNER_TIME_BENCHMARK_TOLERANCE || '') ?? 5_000;
 // #endregion module
